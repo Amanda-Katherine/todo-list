facae59fc9f1fd0b6c2764293e25b9fe0ae23c7c
Annabel Wilmerding <awilmerding@AnnabelngsMBPro.cable.rcn.com>
2020-10-14 12:51:18 -0400

    adds alternative to ListSerializer for todos relationship

--- todo-list-backend/app/serializers/list_serializer.rb
+++ todo-list-backend/app/serializers/list_serializer.rb
@@ -1,5 +1,8 @@
 class ListSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name
+  # attribute :todos do |t|
+  #   TodoSerializer.new(t.todos).as_json["data"]
+  # end
   has_many :todos
 end
